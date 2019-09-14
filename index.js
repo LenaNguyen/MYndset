7a8c941e39ce09b204b35b18eca4e11a135f5a25
LenaNguyen27 <nguyen.lena27@hotmail.com>
2019-09-14 10:03:15 -0400

    init files

---
+++ index.js
@@ -0,0 +1,33 @@
+const CognitiveServicesCredentials = require("@azure/ms-rest-js");
+const TextAnalyticsAPIClient = require("@azure/cognitiveservices-textanalytics");
+
+const key_var = 'TEXT_ANALYTICS_SUBSCRIPTION_KEY';
+if (!process.env[key_var]) {
+	throw new Error('please set/export the following environment variable: ' + key_var);
+}
+const subscription_key = process.env[key_var];
+
+const endpoint_var = 'TEXT_ANALYTICS_ENDPOINT';
+if (!process.env[endpoint_var]) {
+	throw new Error('please set/export the following environment variable: ' + endpoint_var);
+}
+const endpoint = process.env[endpoint_var];
+
+const creds = new CognitiveServicesCredentials.ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': subscription_key } });
+const client = new TextAnalyticsAPIClient.TextAnalyticsClient(creds, endpoint);
+
+const inputDocuments = {
+	documents: [
+		{ language: "en", id: "1", text: "I had the best day of my life." }
+	]
+}
+
+const operation = client.sentiment({ multiLanguageBatchInput: inputDocuments })
+operation
+	.then(result => {
+		console.log(result.documents);
+	})
+	.catch(err => {
+		throw err;
+	});
+
